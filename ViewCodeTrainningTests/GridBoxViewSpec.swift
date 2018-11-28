e68b9f652e8fc691b36588ef1e117c1dce9de93b
adannsergio <adannsergio@gmail.com>
2018-11-28 04:36:28 -0200

    Created QuickSpec Snippet

---
+++ ViewCodeTrainningTests/GridBoxViewSpec.swift
@@ -0,0 +1,9 @@
+//
+//  GridBoxViewSpec.swift
+//  ViewCodeTrainningTests
+//
+//  Created by Adann Simões on 28/11/18.
+//  Copyright © 2018 thiago.lioy. All rights reserved.
+//
+
+import Foundation
