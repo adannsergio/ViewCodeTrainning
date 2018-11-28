61d29787d1b9c2bde97daac8d25ad37b53f39a34
adannsergio <adannsergio@gmail.com>
2018-11-28 03:27:16 -0200

    Button constraint refactoring with SnapKit

--- ViewCodeTrainning/ViewControllerScreen.swift
+++ ViewCodeTrainning/ViewControllerScreen.swift
@@ -16,8 +16,7 @@ final class ViewControllerScreen: UIView {
         let view = UIButton(frame: .zero)
         view.backgroundColor = .red
         view.setTitle("Fetch", for: .normal)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        
+        //view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
@@ -37,10 +36,19 @@ extension ViewControllerScreen: CodeView {
     }
     
     func setupConstraint() {
-        button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
-        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -15).isActive = true
+//        button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+//        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
+//        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+//        button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -15).isActive = true
+        
+        button.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(15)
+            //make.right.equalToSuperview().offset(-15)
+            make.right.equalToSuperview().inset(15)
+            make.height.equalTo(50)
+            make.bottom.equalTo(self).inset(15)
+            
+        }
     }
     
     func setupAdditionalConfiguration() {
